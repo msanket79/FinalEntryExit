e71b430db63d14a42a490f0eaab782cbd9f60549
Mourya-K <mourya.k2112@gmail.com>
2023-06-18 16:48:59 +0530

    Made Responsive

---
+++ name/src/context/SharingContext.js
@@ -0,0 +1,39 @@
+import { useState, createContext } from "react";
+
+const SharingContext = createContext();
+
+function SharingProvider({ children }) {
+  const [show, setShow] = useState(true);
+  const [role, setRole] = useState("");
+  const [isAuth, setAuth] = useState(false);
+  const [ID, setID] = useState("");
+  const [APIaddr, setAPIaddr] = useState("http://10.0.9.243:8000/");
+  const [access, setAccess] = useState([]);
+  const [curRole, setCurRole] = useState();
+
+  return (
+    <SharingContext.Provider
+      value={{
+        show,
+        setShow,
+        role,
+        setRole,
+        isAuth,
+        setAuth,
+        ID,
+        setID,
+        APIaddr,
+        setAPIaddr,
+        access,
+        setAccess,
+        curRole,
+        setCurRole,
+      }}
+    >
+      {children}
+    </SharingContext.Provider>
+  );
+}
+
+export { SharingProvider };
+export default SharingContext;
